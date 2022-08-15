2fe054d2a041780533bfca7ce3dade4456041d35
MatheusCabralAraujo <matheuscabral20114@gmail.com>
2022-08-15 19:35:31 -0300

    Refatora requisito 12

--- src/services/post.service.js
+++ src/services/post.service.js
@@ -44,8 +44,9 @@ const createPost = async ({ title, content, categoryIds, userId }) => {
     published: Date.now(),
     updated: Date.now(),
   });
-  categoryIds.forEach(async (categoryId) => 
-  PostCategory.bulkCreate([{ postId: post.id, categoryId }]));
+  categoryIds.forEach(async (categoryId) => {
+  await PostCategory.create([{ postId: post.id, categoryId }]); 
+});
   
   return post;
 };
